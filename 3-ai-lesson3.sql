0bf2070d11b397b92dd8dcf59b5850ddae1117f5
Jakub Josef Forman <8530188+JayJay666@users.noreply.github.com>
2021-10-05 14:19:17 +0200

    Update 3-ai-lesson3.sql

--- 3-ai-lesson3.sql
+++ 3-ai-lesson3.sql
@@ -28,3 +28,31 @@ ADD nick VARCHAR(30) DEFAULT "NEJ CAR"; -- AFTER COLUMN id;
 DROP TABLE cars;
 
 SELECT * FROM cars;
+
+
+-- ZÁKLADNÍ PRÁCE SE SELECT
+
+CREATE TABLE cars (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL,
+    age INT NULL DEFAULT NULL,
+    content TEXT NULL DEFAULT NULL
+);
+
+INSERT INTO cars (content, name, age) VALUES("Nejlepšé auto xxxx", "BMW X5", 10);
+INSERT INTO cars (content, name, age) VALUES("Nejhorší auto xxxx", "BMW X3", 5);
+INSERT INTO cars (content, name, age) VALUES("Nejlepšé auto xxxx", "BMW X1", 10);
+INSERT INTO cars (content, name, age) VALUES("Nejhorší auto xxxx", "BMW X3", 5);
+INSERT INTO cars (content, name, age) VALUES("Nejlepšé auto xxxx", "BMW X5", 10);
+INSERT INTO cars (content, name, age) VALUES("Nejhorší auto xxxx", "BMW X2", 5);
+INSERT INTO cars (content, name, age) VALUES("Nejlepšé auto xxxx", "BMW X5", 10);
+INSERT INTO cars (content, name, age) VALUES("Nejhorší auto xxxx", "BMW X3", 5);
+
+-- SELECT * FROM cars ORDER BY id DESC;
+-- SELECT * FROM cars ORDER BY id ASC;
+
+-- SELECT * FROM cars ORDER BY id DESC LIMIT 2 OFFSET 2;
+
+-- SELECT * FROM cars LIMIT 2 OFFSET 2;
+-- SELECT * FROM cars WHERE name IN ("BMW X5","BMW X1","BMW X2");
+SELECT COUNT(*) FROM cars;
