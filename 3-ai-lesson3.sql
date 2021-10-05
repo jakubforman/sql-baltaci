2a7459452d8d3f836ebd2cd4aa7e665fa855f176
Jakub Josef Forman <8530188+JayJay666@users.noreply.github.com>
2021-10-05 14:03:38 +0200

    Create 3-ai-lesson3.sql

---
+++ 3-ai-lesson3.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+    rd INT NOT NULL, -- PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    age TEXT NULL DEFAULT NULL,
+    height FLOAT NULL DEFAULT NULL,
+    PRIMARY KEY(RD)
+);
+
+CREATE TABLE cars (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL,
+    age INT NULL DEFAULT NULL,
+    content TEXT NULL DEFAULT NULL
+);
+
+INSERT INTO users (rd, height, name, age) VALUES(0206994444, 167.5, "Tonda", "199 let");
+INSERT INTO users (rd, height, name, age) VALUES(0206994454, 187.5, "Jana", "59 let");
+
+INSERT INTO cars (content, name, age) VALUES("Nejlepšé auto xxxx", "BMW X5", 10);
+INSERT INTO cars (content, name, age) VALUES("Nejhorší auto xxxx", "BMW X3", 5);
+
+ALTER TABLE cars
+ADD nick VARCHAR(30) DEFAULT "NEJ CAR"; -- AFTER COLUMN id;
+
+-- ALTER TABLE cars
+-- DROP COLUMN age;
+
+DROP TABLE cars;
+
+SELECT * FROM cars;
