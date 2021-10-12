5c792fe4c761c1574404d84f073255b20f7b17a1
Jakub Josef Forman <8530188+JayJay666@users.noreply.github.com>
2021-10-12 14:15:31 +0200

    Update music-app.sql

--- music-app.sql
+++ music-app.sql
@@ -1,36 +1,150 @@
-CREATE TABLE songs (
-  id 			    INT 			NOT NULL 		PRIMARY KEY 	AUTO_INCREMENT,
-  id_author		INT 			NOT NULL,
-  id_genre		INT 			NOT NULL,
-  duration 		TIME 			NOT NULL		DEFAULT '00:00:00',
-  name			  VARCHAR(255)	NOT NULL,
-  year			  INT(4)							DEFAULT NULL,
-  file			  VARCHAR(255)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Úte 12. říj 2021, 14:11
+-- Verze serveru: 10.4.21-MariaDB
+-- Verze PHP: 8.0.11
 
-CREATE TABLE authors (
-  id			    INT				NOT NULL		PRIMARY KEY 	AUTO_INCREMENT,
-  first_name	VARCHAR(255)	NOT NULL,
-  last_name		VARCHAR(255)	NOT NULL,
-  birth			  DATE			NOT NULL,
-  death 		  DATE			NOT NULL,
-  bio			    TEXT							DEFAULT NULL,
-  image			  VARCHAR(255)					DEFAULT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE genres (
-  id			  INT				NOT NULL		PRIMARY KEY 	AUTO_INCREMENT,
-  name			VARCHAR(255)	NOT NULL
-);	
 
-ALTER TABLE songs 
-ADD FOREIGN KEY (id_author) 
-REFERENCES authors(id);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-ALTER TABLE songs 
-ADD FOREIGN KEY (id_genre) 
-REFERENCES genres(id);
-    
-    
--- insert
-INSERT INTO `genres` (`id`, `name`) VALUES (NULL, 'nu-meta'), (NULL, 'metalcore');
+--
+-- Databáze: `ut_music`
+--
+CREATE DATABASE IF NOT EXISTS `ut_music` DEFAULT CHARACTER SET utf8 COLLATE utf8_czech_ci;
+USE `ut_music`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `created` date NOT NULL,
+  `end` date DEFAULT NULL,
+  `bio` text COLLATE utf8_czech_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `authors`
+--
+
+INSERT INTO `authors` (`id`, `name`, `created`, `end`, `bio`, `image`) VALUES
+(2, 'Slipknot', '1993-01-01', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `genres`
+--
+
+INSERT INTO `genres` (`id`, `name`) VALUES
+(1, 'nu-meta'),
+(2, 'metalcore');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `songs`
+--
+
+CREATE TABLE `songs` (
+  `id` int(11) NOT NULL,
+  `id_author` int(11) NOT NULL,
+  `id_genre` int(11) NOT NULL,
+  `duration` time NOT NULL DEFAULT '00:00:00',
+  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `year` int(4) DEFAULT NULL,
+  `file` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `songs`
+--
+
+INSERT INTO `songs` (`id`, `id_author`, `id_genre`, `duration`, `name`, `year`, `file`) VALUES
+(1, 2, 1, '00:00:00', 'Spit It Out', 1999, 'https://open.spotify.com/track/4Y2LMfn1BTdcvnO9cmJWU6?autoplay=true');
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pro tabulku `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pro tabulku `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_author` (`id_author`),
+  ADD KEY `id_genre` (`id_genre`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pro tabulku `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pro tabulku `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Omezení pro exportované tabulky
+--
+
+--
+-- Omezení pro tabulku `songs`
+--
+ALTER TABLE `songs`
+  ADD CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `authors` (`id`),
+  ADD CONSTRAINT `songs_ibfk_2` FOREIGN KEY (`id_genre`) REFERENCES `genres` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
